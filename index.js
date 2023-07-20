82ccbd759fc9908a5feebb07a2234348f837b3ac
chaya <chayavogel1@gmail.com>
2023-07-20 09:38:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Chaya Vogel",
+    streetAddress: "440 Brooklyn Ave",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = {
+        ...object,
+        [key]: value,
+    };
+    return newEmployee;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+ function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+ }
